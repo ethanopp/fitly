1bc4a20460adf4b185d06f4218a3e1edce430aee
ethan <ethanopp@gmail.com>
2020-12-30 12:44:51 -0500

    Function for querying tracks during workout

--- src/fitly/api/spotifyAPI.py
+++ src/fitly/api/spotifyAPI.py
@@ -4,7 +4,7 @@ import json
 from ..utils import config
 from ..app import app
 from ..api.database import engine
-from ..api.sqlalchemy_declarative import apiTokens, spotifyPlayHistory
+from ..api.sqlalchemy_declarative import apiTokens, spotifyPlayHistory, stravaSummary
 from sqlalchemy import delete, func
 from datetime import datetime
 import ast
@@ -120,7 +120,6 @@ def save_spotify_play_history():
         track_table = pd.merge(track_info_df, track_features, how='left', left_on='track_id', right_on='id').set_index(
             'played_at')
         track_table.drop_duplicates(inplace=True)
-
         track_table = track_table.drop(columns=['id', 'type', 'uri'])
 
         # Filter to only new records and insert into DB
@@ -133,16 +132,57 @@ def save_spotify_play_history():
             app.server.logger.debug(f'{len(track_table)} new songs found!')
             track_table.to_sql('spotify_play_history', engine, if_exists='append', index=True)
 
-# def get_recommendations():
-#     '''
-#     Queries the users play history table and passes filters specified by user in UI for recommendations
-#     :return:
-#     '''
-#     # TODO: Finish writing this function
-#     if spotify_connected():
-#         spotify = get_spotify_client()
-#             # Recommendations
-#             play_history = session.query()
-#
-#             recommendations = spotify.recommendations(track_ids=top_track_ids, limit=50).tracks
-#             spotify.recommendations()
+
+def get_workout_tracks(workout_intensity=None, sport=None):
+    '''
+
+    :param workout_intensity: (Optional) Filters the spotify tracks by the intensity of the workout that was done
+    :return: df of spotify tracks that were done during a workout
+    '''
+    # Query tracks and workouts
+    df_summary = pd.read_sql(
+        sql=app.session.query(stravaSummary.start_date_utc, stravaSummary.activity_id, stravaSummary.name,
+                              stravaSummary.elapsed_time, stravaSummary.type,
+                              stravaSummary.workout_intensity).statement, con=engine)
+    df_summary['end_date_utc'] = df_summary['start_date_utc'] + pd.to_timedelta(df_summary['elapsed_time'], 's')
+    df_summary.drop(columns=['elapsed_time'], inplace=True)
+    df_tracks = pd.read_sql(sql=app.session.query(spotifyPlayHistory).statement, con=engine)
+    # Full Cross Join
+    df_tracks = df_tracks.assign(key=1)
+    df_summary = df_summary.assign(key=1)
+    df_merge = pd.merge(df_summary, df_tracks, on='key').drop('key', axis=1)
+    # Filter only on tracks performed during workout times
+    df_merge = df_merge.query('played_at >= start_date_utc and played_at <= end_date_utc')
+    # Join back to original date range table and drop key column
+    df = df_tracks.merge(df_merge, on=['played_at'], how='left').fillna('').drop('key', axis=1)
+    # Cleanup the end resulting df
+    df = df[[c for c in df.columns if '_y' not in c]]
+    df.columns = [c.replace('_x', '') for c in df.columns]
+    df = df.rename(columns={'type': 'workout_type', 'name': 'workout_name'})
+    df = df[df['start_date_utc'] != '']
+    # If workout intensity passed, filter on it
+    if workout_intensity:
+        df = df[df['workout_intensity'] == workout_intensity]
+    if sport:
+        df = df[df['workout_type'] == workout_intensity]
+
+    df.drop(columns=['start_date_utc', 'end_date_utc'], inplace=True)
+
+    df.set_index('played_at', inplace=True)
+
+    return df
+
+
+def get_recommendations():
+    '''
+    Queries the users play history table and passes filters specified by user in UI for recommendations
+    :return:
+    '''
+    # TODO: Finish writing this function
+    if spotify_connected():
+        spotify = get_spotify_client()
+        # Recommendations
+        play_history = app.session.query(spotifyPlayHistory)
+
+        recommendations = spotify.recommendations(track_ids=top_track_ids, limit=50).tracks
+        spotify.recommendations()
